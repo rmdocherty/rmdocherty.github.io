6cfcafb6601016fec160a35a7b31db7e73b58df3
rmdocherty <rmdocherty@users.noreply.github.com>
2017-02-02 19:25:16 +0000

    removed unnecessary console.log

--- screenCheck.js
+++ screenCheck.js
@@ -9,7 +9,7 @@ if ( screen.width > 500)
 {
 loadCSS("Ed-Aid.css")
 }else{
-	console.log(1)
+
 document.styleSheets[2].disabled = true;
 document.styleSheets[(document.styleSheets.length -1)].disabled = true;
 loadCSS("Mobile Ed-Aid.css")
