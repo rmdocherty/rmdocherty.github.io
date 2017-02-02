abc8aea591d505bf11f4ecf7833acbe5629aa4a0
rmdocherty <rmdocherty@users.noreply.github.com>
2017-02-02 19:14:12 +0000

    fixed stylesheets links

--- screenCheck.js
+++ screenCheck.js
@@ -12,5 +12,5 @@ loadCSS("stylesheets/Ed-Aid.css")
 	console.log(1)
 document.styleSheets[2].disabled = true;
 document.styleSheets[(document.styleSheets.length -1)].disabled = true;
-loadCSS("stylesheets/Mobile Ed-Aid.css")
-loadCSS("stylesheets/MobileMove.css")}
+loadCSS("Mobile Ed-Aid.css")
+loadCSS("MobileMove.css")}
